3c6c56d0da3b84a37322b75135bdc651c82fc84d
IlyaAnik <71770143+IlyaAnik@users.noreply.github.com>
2020-09-30 16:54:40 +0300

    Add files via upload

---
+++ laba_2.1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{ 
+    int a,b,c;
+     printf("Введите переменную a:");
+     scanf("%d",&a);
+     printf("Введите переменную b:");
+     scanf("%d",&b);
+     printf("Введите переменную c:");
+     scanf("%d",&c);
+     if (a<b && a<c) printf("Yes");
+     else if(a>b && a>c) printf("No");
+
+    
+
+}
